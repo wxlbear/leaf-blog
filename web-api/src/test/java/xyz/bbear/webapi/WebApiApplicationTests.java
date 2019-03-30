50ca42c588d3928383b4d1a6216356ace0d3d6c9
xiongliu.wu <xiongliu.wu@500px.com>
2019-03-30 21:42:17 +0800

    [ADD] check style

--- web-api/src/test/java/xyz/bbear/webapi/WebApiApplicationTests.java
+++ web-api/src/test/java/xyz/bbear/webapi/WebApiApplicationTests.java
@@ -1,16 +1,14 @@
 package xyz.bbear.webapi;
 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebApiApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+  @Test
+  public void contextLoads() {}
 }
