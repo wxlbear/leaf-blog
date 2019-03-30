b881073a176b602a629a4035ef1d171b445de354
xiongliu.wu <xiongliu.wu@500px.com>
2019-03-30 22:27:42 +0800

    [MOD]

--- leaf-api/src/test/java/xyz/bbear/api/ApplicationTest.java
+++ leaf-api/src/test/java/xyz/bbear/api/ApplicationTest.java
@@ -1,13 +1,18 @@
-package com.example.demo;
+package xyz.bbear.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * ApplicationTest.
+ *
+ * @author xiongliu wu 2019-03-30 22:25
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class ApplicationTest {
 
   @Test
   public void contextLoads() {}
