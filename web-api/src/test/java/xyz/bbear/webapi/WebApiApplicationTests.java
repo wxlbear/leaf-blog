4c44080ce8679751bfb84a68fb42cf622c83985d
xiongliu.wu <xiongliu.wu@500px.com>
2019-03-30 19:01:39 +0800

    [INIT] init the project and travis yml

---
+++ web-api/src/test/java/xyz/bbear/webapi/WebApiApplicationTests.java
@@ -0,0 +1,17 @@
+package xyz.bbear.webapi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
