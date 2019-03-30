4cccfec84cd7218467bf18d28dd020c7c469155a
xiongliu.wu <xiongliu.wu@500px.com>
2019-03-30 22:13:48 +0800

    [ADD] check style

--- leaf-api/src/main/java/xyz/bbear/api/Application.java
+++ leaf-api/src/main/java/xyz/bbear/api/Application.java
@@ -1,12 +1,16 @@
-package xyz.bbear.webapi;
+package xyz.bbear.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application.
+ *
+ * @author xiongliu wu 2019-03-30 22:07
+ */
 @SpringBootApplication
-public class WebApiApplication {
-
+public class Application {
   public static void main(String[] args) {
-    SpringApplication.run(WebApiApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
---
+++ leaf-common/src/main/java/xyz/bbear/common/utils/TestUtils.java
@@ -0,0 +1,8 @@
+package xyz.bbear.common.utils;
+
+/**
+ * TestUtils.
+ *
+ * @author xiongliu wu 2019-03-30 22:04
+ */
+public class TestUtils {}
